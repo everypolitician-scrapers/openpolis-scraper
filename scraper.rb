e5643744ff72eb27fc7a210f11dcd4c96319c8c3
Tony Bowden <tony@tmtm.com>
2015-03-20 08:13:17 +0000

    Extract the ID and surname

--- scraper.rb
+++ scraper.rb
@@ -19,11 +19,13 @@ end
 sources.each do |house, url|
   noko = noko_for(url)
   noko.css('div.genericblock table tr:nth-child(n+3)').each do |tr|
-    name,group,area = tr.css('td').map { |td| td.text.strip }
+    info = tr.css('td')
     data = { 
-      name: name,
-      group: group,
-      area: area,
+      id: info[0].at_css('a')['href'].split('/').last,
+      name: info[0].text.strip,
+      surname: info[0].at_css('.surname').text.strip,
+      group: info[1].text.strip,
+      area: info[2].text.strip,
       house: house,
     }
     puts data
