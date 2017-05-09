5aed773b402199034239e38bd9b881378794f5fa
Tony Bowden <tony@tmtm.com>
2017-05-09 06:43:06 +0100

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/ruby
+# frozen_string_literal: true
 
 # Convert OpenPolis lists to CSV
 
@@ -10,14 +11,14 @@ require 'pry'
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
-sources = { 
-  'Senate' => 'http://politici.openpolis.it/istituzione/senatori/5',
+sources = {
+  'Senate'              => 'http://politici.openpolis.it/istituzione/senatori/5',
   'Chamber of Deputies' => 'http://politici.openpolis.it/istituzione/deputati/4',
 }
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -28,10 +29,10 @@ end
 def scrape_person(url)
   noko = noko_for(url)
   data = {
-    image: noko.css('img#foto_politico/@src').text,
-    email: noko.css('div.contacts a[href*="mailto:"]/@href').map { |u| u.value.gsub('mailto:','') }.uniq.join(';'),
-    facebook: noko.css('div.contacts a[href*="facebook"]/@href').map { |u| u.value }.uniq.join(';'),
-    twitter: noko.css('div.contacts a[href*="twitter"]/@href').map { |u| u.value }.uniq.join(';'),
+    image:    noko.css('img#foto_politico/@src').text,
+    email:    noko.css('div.contacts a[href*="mailto:"]/@href').map { |u| u.value.gsub('mailto:', '') }.uniq.join(';'),
+    facebook: noko.css('div.contacts a[href*="facebook"]/@href').map(&:value).uniq.join(';'),
+    twitter:  noko.css('div.contacts a[href*="twitter"]/@href').map(&:value).uniq.join(';'),
   }
 
   # TODO: This is a workaround for mailto: links containing YouTube links it
@@ -48,15 +49,15 @@ sources.each do |house, url|
   noko.css('div.genericblock table tr:nth-child(n+3)').each do |tr|
     tds = tr.css('td')
     link = URI.join(url, tds[0].css('a/@href').text).to_s
-    data = { 
-      id: link.split('/').last,
-      name: tds[0].text.tidy,
+    data = {
+      id:      link.split('/').last,
+      name:    tds[0].text.tidy,
       surname: tds[0].at_css('.surname').text.tidy,
-      party: tds[1].text.tidy,
-      area: tds[2].text.tidy,
-      house: house,
-      term: 17,
-      source: link,
+      party:   tds[1].text.tidy,
+      area:    tds[2].text.tidy,
+      house:   house,
+      term:    17,
+      source:  link,
     }.merge(scrape_person(link))
     ScraperWiki.save_sqlite([:id], data)
   end
